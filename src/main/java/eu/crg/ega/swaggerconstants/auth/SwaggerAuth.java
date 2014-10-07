35ebe338ecfc16805201dc873da27c5e072f768c
Sabela de la Torre <sabela.delatorre@crg.eu>
2014-10-07 16:15:18 +0200

    Add constants that will be used in swagger annotations.

---
+++ src/main/java/eu/crg/ega/swaggerconstants/auth/SwaggerAuth.java
@@ -0,0 +1,36 @@
+package eu.crg.ega.swaggerconstants.auth;
+
+public class SwaggerAuth {
+
+  public static final String DESCRIPTION = "Authentication operations";
+  
+  public static final String USER_LOGIN = "Login";
+  public static final String USER_LOGIN_NOTES = "";
+  public static final String USER_LOGIN_USERNAME = "";
+  public static final String USER_LOGIN_PASSWORD = "";
+  public static final String USER_LOGIN_LOGIN_TYPE = "";
+
+  public static final String USER_LOGIN_TYPES_NOTES = "";
+  public static final String USER_LOGIN_TYPES = "List login types";
+
+  public static final String DATSETS_NOTES = "";
+  public static final String DATASETS = "List datasets";
+  public static final String DATASETS_USERNAME = "";
+  public static final String DATASETS_RESOURCE = "Dataset stable Id, set this value to check permissions on this dataset";
+  public static final String DATASETS_PARAMS = "";
+
+  public static final String PACKETS_NOTES = "";
+  public static final String PACKETS = "List packets";
+  public static final String PACKETS_USERNAME = "";
+  public static final String PACKETS_RESOURCE = "Dataset Id";
+  public static final String PACKETS_PARAMS = "";
+
+  public static final String FILES_NOTES = "";
+  public static final String FILES = "List files";
+  public static final String FILES_USERNAME = "";
+  public static final String FILES_RESOURCE = "Packet Id";
+  public static final String FILES_PARAMS = "";
+  
+  
+  
+}
---
+++ src/main/java/eu/crg/ega/swaggerconstants/session/SwaggerSession.java
@@ -0,0 +1,40 @@
+package eu.crg.ega.swaggerconstants.session;
+
+public class SwaggerSession {
+  
+  public static final String DESCRIPTION = "Session management";
+
+  public static final String LOGIN = "Login";
+  public static final String LOGIN_NOTES = "";
+  
+  public static final String LOGIN_TYPES_NOTES = "";
+  public static final String LOGIN_TYPES = "Login types";
+  public static final String LOGIN_USERNAME = "";
+  public static final String LOGIN_PASSWORD = "";
+  public static final String LOGIN_LOGIN_TYPE = "";
+  
+  public static final String LOGOUT_NOTES = "";
+  public static final String LOGOUT = "Logout";
+  public static final String LOGOUT_SESSION_ID = "";
+  
+  public static final String LIST_SESSIONS = "List sessions";
+  public static final String LIST_SESSIONS_NOTES = "";
+  public static final String LIST_SESSIONS_PARAMS = "";
+  
+  public static final String LIST_SESSIONS_STATUS = "Sessions status";
+  public static final String LIST_SESSIONS_STATUS_NOTES = "List all possible status of sessions";
+  
+  public static final String CHECK_NOTES = "";
+  public static final String CHECK = "Check session status";
+  
+  public static final String CHECK_SESSION_ID = "Check session status by Id";
+  public static final String CHECK_SESSION_ID_NOTES = "Checks the status of the session with this Id";
+  public static final String CHECK_SESSION_ID_SESSION_ID = "";
+  
+  public static final String PERMISSIONS = "Get permissions";
+  public static final String PERMISSIONS_NOTES = "";
+  public static final String PERMISSIONS_SESSION_ID = "";
+  public static final String PERMISSIONS_RESOURCE = "";
+
+
+}
