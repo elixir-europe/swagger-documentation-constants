1e7487afc9914766045f46709c759923185a1f27
Sabela de la Torre <sabela.delatorre@crg.eu>
2014-10-07 13:21:10 +0200

    Add some messages.

--- src/main/java/eu/crg/ega/swaggerconstants/common/SwaggerBase.java
+++ src/main/java/eu/crg/ega/swaggerconstants/common/SwaggerBase.java
@@ -5,37 +5,37 @@ public class SwaggerBase {
   // Instrumentation
   public static final String DESCRIPTION = "Common operations to all services";
   
-  public static final String GET_STATUS = "";
+  public static final String GET_STATUS = "Get status";
   public static final String GET_STATUS_NOTES = "";
   
   public static final String SET_STATUS_NOTES = "";
-  public static final String SET_STATUS = "";
+  public static final String SET_STATUS = "Set status";
   public static final String SET_STATUS_VALUE = "";
   
   public static final String GET_STATUS_STATES_NOTES = "";
-  public static final String GET_STATUS_STATES = "";
+  public static final String GET_STATUS_STATES = "Get status states";
   
   public static final String GET_COMMANDS_NOTES = "";
-  public static final String GET_COMMANDS = "";
+  public static final String GET_COMMANDS = "Get commands";
 
   // Log
-  public static final String SET_LOG_LEVEL = "";
+  public static final String SET_LOG_LEVEL = "Set log level";
   public static final String SET_LOG_LEVEL_NOTES = "";
   public static final String SET_LOG_LEVEL_VALUE = "";
 
-  public static final String GET_LOG_LEVEL = "";
+  public static final String GET_LOG_LEVEL = "Get log level";
   public static final String GET_LOG_LEVEL_NOTES = "";
 
-  public static final String LEVELS = "";
+  public static final String LEVELS = "Get available levels";
   public static final String LEVELS_NOTES = "";
 
   // Metrics
   public static final String RESET_NOTES = "";
-  public static final String RESET = "";
+  public static final String RESET = "Reset";
 
   // Events
-  public static final String EVENTS_NOTES = "";
-  public static final String EVENTS = "";
+  public static final String EVENTS_NOTES = "Send an event to the service";
+  public static final String EVENTS = "Event";
   public static final String EVENTS_EVENT = "";
   public static final String EVENTS_REPLY_TO = "";
   public static final String EVENTS_TYPE = "";
