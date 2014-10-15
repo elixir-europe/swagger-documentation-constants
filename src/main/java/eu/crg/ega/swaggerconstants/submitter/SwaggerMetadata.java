fddeb72744cb79cb1487c2db679b18c753fdedfb
Sabela de la Torre <sabela.delatorre@crg.eu>
2014-10-15 14:49:54 +0200

    Add constants used by submitter portal.

--- src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadata.java
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadata.java
@@ -4,4 +4,41 @@ public class SwaggerMetadata {
 
   public static final String DESCRIPTION = "Operations over metadata information";
   
+  public static final String LIST_EXPERIMENTS_NOTES = "";
+  public static final String LIST_EXPERIMENTS = "List submission experiments";
+  
+  public static final String EXPERIMENTS = "Submit a new experiment";
+  public static final String EXPERIMENTS_NOTES = "";
+
+  public static final String LIST_STUDIES_NOTES = "";
+  public static final String LIST_STUDIES = "List submission studies";
+  
+  public static final String STUDIES = "Submit a new study";
+  public static final String STUDIES_NOTES = "";
+  public static final String STUDY_DATA = "";
+
+  public static final String LIST_SAMPLES = "List submission samples";
+  public static final String LIST_SAMPLES_NOTES = "";
+
+  public static final String SAMPLES = "Submit a new sample";
+  public static final String SAMPLES_NOTES = "";
+
+  public static final String LIST_DATASETS = "List submission datasets";
+  public static final String LIST_DATASETS_NOTES = "";
+
+  public static final String SUBMIT_DATASETS_NOTES = "";
+  public static final String SUBMIT_DATASETS = "Submit a new dataset";
+
+  public static final String LIST_POLICIES = "List submission policies";
+  public static final String LIST_POLICIES_NOTES = "";
+
+  public static final String SUBMIT_POLICIES = "Submit a new policy";
+  public static final String SUBMIT_POLICIES_NOTES = "Create one or more policies.";
+
+  public static final String LIST_DACS = "List submission DACs";
+  public static final String LIST_DACS_NOTES = "";
+
+  public static final String SUBMIT_DACS_NOTES = "Create one or more DACs.";
+  public static final String SUBMIT_DACS = "Submit a new DAC";
+  
 }
--- src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerSubmission.java
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerSubmission.java
@@ -3,38 +3,17 @@ package eu.crg.ega.swaggerconstants.submitter;
 public class SwaggerSubmission {
 
   public static final String DESCRIPTION = "Operations related to a submission";
-  
-  public static final String LIST_LIBRARY_STRATEGY = "Library Strategy accepted values";
-  public static final String LIST_LIBRARY_STRATEGY_NOTES = "Valid values for the Experiment XML field called LIBRARY_STRATEGY.";
-  
-  public static final String LIST_LIBRARY_SELECTION = "Library Selection accepted values";
-  public static final String LIST_LIBRARY_SELECTION_NOTES = "Valid values for the Experiment XML field called LIBRARY_SELECTION.";
-  
-  public static final String LIST_LIBRARY_SOURCE = "Library source accepted values";
-  public static final String LIST_LIBRARY_SOURCE_NOTES = "Valid values for the Experiment XML field called LIBRARY_SOURCE.";
-  
-  public static final String LIST_INSTRUMENT_MODEL = "Platform accepted values";
-  public static final String LIST_INSTRUMENT_MODEL_NOTES = "Valid values for the Experiment XML field called PLATFORM.";
 
-  public static final String LIST_DATASET_TYPE = "Dataset type accepted values";
-  public static final String LIST_DATASET_TYPE_NOTES = "Valid values for the Dataset XML field DATASET_TYPE.";
-  
   public static final String SUBMISSION_STATUS = "Submission status";
   public static final String SUBMISSION_STATUS_DESCRIPTION = "";
-
-  public static final String LIST_STUDY_TYPE_NOTES = "Study type accepted values";
-  public static final String LIST_STUDY_TYPE = "Valid values for the Study XML field STUDY_TYPE.";
-
-  public static final String STUDY = "Submit a new study";
-  public static final String STUDY_NOTES = "";
-  public static final String STUDY_DATA = "";
-
+  
   public static final String LIST_STUDIES = "List studies";
   public static final String LIST_STUDIES_NOTES = "List all studies uploaded by the user logged in.";
   
   public static final String NEW_SUBMISSION = "New submission";
   public static final String NEW_SUBMISSION_NOTES = "Returns the Id for the new submission.";
-  public static final String NEW_SUBMISSION_DESCRIPTION = "";
+  public static final String NEW_SUBMISSION_TITLE = "Submission title";
+  public static final String NEW_SUBMISSION_DESCRIPTION = "Submission long description";
 
   public static final String GET_SUBMISSION = "View submission";
   public static final String GET_SUBMISSION_NOTES = "Retrieves an existing submission by Id";
