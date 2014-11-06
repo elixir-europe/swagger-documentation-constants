42d894fac9dad282a76a09dbf75db16a7b26276c
Sabela de la Torre <sabela.delatorre@crg.eu>
2014-11-06 18:06:24 +0100

    New constants used in submitter portal.

--- src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadataEdit.java
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadataEdit.java
@@ -24,4 +24,28 @@ public class SwaggerMetadataEdit {
   public static final String EDIT_DAC = "Modify existing DAC";
   public static final String EDIT_DAC_NOTES = "";
 
+  public static final String EDIT_EXPERIMENT_VIA_XML = "Modify existing experiment using an XML as input";
+  public static final String EDIT_EXPERIMENT_VIA_XML_NOTES = "";
+  public static final String EDIT_EXPERIMENT_VIA_XML_XML = "XML content";
+
+  public static final String EDIT_STUDY_VIA_XML = "Modify existing study using an XML as input";
+  public static final String EDIT_STUDY_VIA_XML_NOTES = "";
+  public static final String EDIT_STUDY_VIA_XML_XML = "XML content";
+
+  public static final String EDIT_SAMPLE_VIA_XML = "Modify existing sample using an XML as input";
+  public static final String EDIT_SAMPLE_VIA_XML_NOTES = "";
+  public static final String EDIT_SAMPLE_VIA_XML_XML = "XML content";
+
+  public static final String EDIT_DATASET_VIA_XML = "Modify existing dataset using an XML as input";
+  public static final String EDIT_DATASET_VIA_XML_NOTES = "";
+  public static final String EDIT_DATASET_VIA_XML_XML = "XML content";
+
+  public static final String EDIT_POLICY_VIA_XML = "Modify existing policy using an XML as input";
+  public static final String EDIT_POLICY_VIA_XML_NOTES = "";
+  public static final String EDIT_POLICY_VIA_XML_XML = "XML content";
+
+  public static final String EDIT_DAC_VIA_XML = "Modify existing DAC using an XML as input";
+  public static final String EDIT_DAC_VIA_XML_NOTES = "";
+  public static final String EDIT_DAC_VIA_XML_XML = "XML content";
+
 }
--- src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadataSubmit.java
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerMetadataSubmit.java
@@ -27,6 +27,31 @@ public class SwaggerMetadataSubmit {
   public static final String SUBMIT_RUNS = "Submit a new run";
 
   public static final String SUBMIT_EXPERIMENTS_XML_NOTES = "";
-  public static final String SUBMIT_EXPERIMENTS_XML = "Upload an experiment XML file";
-  
+  public static final String SUBMIT_EXPERIMENTS_XML = "Upload an experiment XML";
+  public static final String SUBMIT_EXPERIMENT_XML = "XML content with experiment data";
+
+  public static final String SUBMIT_STUDIES_XML = "Upload a study XML";
+  public static final String SUBMIT_STUDIES_XML_NOTES = "";
+  public static final String SUBMIT_STUDY_XML = "XML content with study data";
+
+  public static final String SUBMIT_DACS_XML = "Upload a DAC XML";
+  public static final String SUBMIT_DACS_XML_NOTES = "";
+  public static final String SUBMIT_DAC_XML = "XML content with DAC data";
+
+  public static final String SUBMIT_POLICIES_XML = "Upload a policy XML";
+  public static final String SUBMIT_POLICIES_XML_NOTES = "";
+  public static final String SUBMIT_POLICY_XML = "XML content with policy data";
+
+  public static final String SUBMIT_DATASETS_XML = "Upload a dataset XML";
+  public static final String SUBMIT_DATASETS_XML_NOTES = "";
+  public static final String SUBMIT_DATASET_XML = "XML content with dataset data";
+
+  public static final String SUBMIT_SAMPLES_XML = "Upload a sample XML";
+  public static final String SUBMIT_SAMPLES_XML_NOTES = "";
+  public static final String SUBMIT_SAMPLE_XML = "XML content with sample data";
+
+  public static final String SUBMIT_RUNS_XML = "Upload a run XML";
+  public static final String SUBMIT_RUNS_XML_NOTES = "";
+  public static final String SUBMIT_RUN_XML = "XML content with run data";
+
 }
