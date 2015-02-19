60b44985024237d3ee9d6af9956a6d6f73316770
Sabela de la Torre <sabela.delatorre@crg.eu>
2015-02-19 18:35:07 +0100

    New constants used in manager portal.

---
+++ src/main/java/eu/crg/ega/swaggerconstants/manager/SwaggerManager.java
@@ -0,0 +1,17 @@
+package eu.crg.ega.swaggerconstants.manager;
+
+public class SwaggerManager {
+  
+  public static final String DESCRIPTION = "System management";
+  
+  public static final String LIST_SESSIONS = "List sessions";
+  public static final String LIST_SESSIONS_NOTES = "";
+  public static final String LIST_SESSIONS_PARAMS = "";
+  
+  public static final String LIST_SESSIONS_STATUS = "Sessions status";
+  public static final String LIST_SESSIONS_STATUS_NOTES = "List all possible status of sessions";
+  
+  public static final String EXPIRE_SESSIONS_NOTES = "";
+  public static final String EXPIRE_SESSIONS = "Expire sessions";
+
+}
