3b053eb283c55ece136ae6bf86e706ac1c7d1fd9
Sabela de la Torre <sabela.delatorre@crg.eu>
2015-03-09 17:18:47 +0100

    New constants.

--- src/main/java/eu/crg/ega/swaggerconstants/auth/SwaggerFtp.java
+++ src/main/java/eu/crg/ega/swaggerconstants/auth/SwaggerFtp.java
@@ -3,5 +3,25 @@ package eu.crg.ega.swaggerconstants.auth;
 public class SwaggerFtp {
 
   public static final String DESCRIPTION = "Ftp users operations";
+
+  public static final String MAP_FTP_USER = "Map FTP user to internal user Id";
+  public static final String MAP_FTP_USER_NOTES = "Retrieve internal user Id using FTP's user Id";
+
+  public static final String NEW_FTP_USER = "Create an FTP account";
+  public static final String NEW_FTP_USER_NOTES =
+      "Create or edit an FTP account. It creates the binding between the requester or submitter user and the FTP account created. ";
+
+  public static final String EDIT_FTP_USER_NOTES = "Edit an FTP account";
+  public static final String EDIT_FTP_USER =
+      "Set password to null if you want to keep the stored one or provide any value to change it "
+          + "(if the value provided does not fulfill password requirements the system will generate one).";
+
+  public static final String LIST_USERS = "List FTP accounts";
+  public static final String LIST_USERS_NOTES = "Set all parameters to null to list all accounts.";
+  public static final String LIST_USERS_LOGIN_TYPE = "List FTP accounts by login type";
+  public static final String LIST_USERS_AUTH_PROVIDER_ID = "List FTP accounts by authentication provider Id";
+
+  public static final String GET_ACCOUNT_NOTES = "Retrieve an account by FTP Box";
+  public static final String GET_ACCOUNT = "Get an account";
   
 }
---
+++ src/main/java/eu/crg/ega/swaggerconstants/manager/SwaggerFtpUser.java
@@ -0,0 +1,21 @@
+package eu.crg.ega.swaggerconstants.manager;
+
+public class SwaggerFtpUser {
+  
+  public static final String DESCRIPTION = "FTP accounts management";
+  
+  public static final String LIST_USERS = "List FTP accounts";
+  public static final String LIST_USERS_NOTES = "Set all parameters to null to list all accounts.";
+  public static final String LIST_USERS_LOGIN_TYPE = "List FTP accounts by login type";
+  public static final String LIST_USERS_AUTH_PROVIDER_ID = "List FTP accounts by authentication provider Id";
+
+  public static final String GET_ACCOUNT_NOTES = "Retrieve an account by FTP Box";
+  public static final String GET_ACCOUNT = "Get an account";
+
+  public static final String EDIT_ACCOUNT = "Edit an account";
+  public static final String EDIT_ACCOUNT_NOTES = "If you want to change the password set an alphanumeric value with a minimum lenght of 8.";
+
+  public static final String NEW_ACCOUNT = "Create an FTP account";
+  public static final String NEW_ACCOUNT_NOTES = "";
+  
+}
