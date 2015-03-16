e69cca01234e97372107bc2ac85d7b09c35738bc
Sabela de la Torre <sabela.delatorre@crg.eu>
2015-03-16 14:59:45 +0100

    New constants.

---
+++ src/main/java/eu/crg/ega/swaggerconstants/manager/SwaggerCatalogManager.java
@@ -0,0 +1,13 @@
+package eu.crg.ega.swaggerconstants.manager;
+
+public class SwaggerCatalogManager {
+
+  public static final String DESCRIPTION = "Catalog related stuff";
+  
+  public static final String LIST_STATUSES_NOTES = "";
+  public static final String LIST_STATUSES = "List audit statuses";
+
+  public static final String LIST_STATUSES_OCCURRENCES_NOTES = "";
+  public static final String LIST_STATUSES_OCCURRENCES = "List audit statuses occurrences";
+  
+}
