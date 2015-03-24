3ba3c0e9d5f9df7e51eb8a63d796035034c3e6eb
Sabela de la Torre <sabela.delatorre@crg.eu>
2015-03-24 17:37:34 +0100

    New constants.

---
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerEbiSubmissionDetails.java
@@ -0,0 +1,13 @@
+package eu.crg.ega.swaggerconstants.submitter;
+
+public class SwaggerEbiSubmissionDetails {
+
+  public static final String DESCRIPTION = "Operations related to EBI submission details";
+ 
+  public static final String LIST = "List all submissions";
+  public static final String LIST_NOTES = "Use user Id to search all EBI submissions done by a user. Use analysis/dac/dataset/experiment/policy/run/sample/study Id to search any EBI submission detail where this Id is found. ";
+
+  public static final String GET = "Get an EBI submission detail";
+  public static final String GET_NOTES = "";
+
+}
---
+++ src/main/java/eu/crg/ega/swaggerconstants/submitter/SwaggerSubmissionCommands.java
@@ -0,0 +1,10 @@
+package eu.crg.ega.swaggerconstants.submitter;
+
+public class SwaggerSubmissionCommands {
+
+  public static final String DESCRIPTION = "Operations related to submission commands";
+
+  public static final String GET = "Get a submission command";
+  public static final String GET_NOTES = "";
+
+}
