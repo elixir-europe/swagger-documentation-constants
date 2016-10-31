92d5bb6187dc9c4ceca379d5fd5fb4c08c0f1e3e
Oscar Martinez <oscar.martinez@crg.eu>
2016-10-31 14:06:19 +0100

    add class to define constants for the version controller

---
+++ src/main/java/eu/crg/ega/swaggerconstants/common/SwaggerVersion.java
@@ -0,0 +1,13 @@
+package eu.crg.ega.swaggerconstants.common;
+
+public class SwaggerVersion {
+
+  public static final String DESCRIPTION = "Version information operations";
+  public static final String GET_CORE_INFO = "Get core version";
+  public static final String GET_CORE_INFO_NOTES = "Obtains version of the core that is bundled with this";
+  public static final String GET_MICROSERVICE_INFO = "Get microservice version";
+  public static final String GET_MICROSERVICE_INFO_NOTES = "";
+  public static final String GET_DEPENDENCIES_INFO = "Get dependencies versions";
+  public static final String GET_DEPENDENCIES_INFO_NOTES = "";
+
+}
